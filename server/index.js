dc2c8c3f7f721a43d8b3dac2be14ab068ab9814f
Danusha Navod Gamage <60852073+wulfri-dng@users.noreply.github.com>
2023-02-17 22:49:09 +0530

    Added comments explaining the code

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Import required dependencies
 const express = require("express");
 const csvParser = require("csv-parser");
 const multer = require('multer');
@@ -5,13 +6,15 @@ const bodyParser = require('body-parser')
 const fs = require("fs");
 
 let fileName;
-const PORT = process.env.PORT || 4000;
+const PORT = 4000; // Set the server port to 4000
 
 const app = express();
 
+// Use body-parser middleware to parse request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Configure Multer storage to save the uploaded CSV file
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -24,12 +27,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// Function to parse the uploaded CSV file into JSON format
 const parseCSV = async () => {
     const records = [];
 
     const parser = fs
-        .createReadStream(`./uploads/${fileName}`)
-        .pipe(csvParser());
+        .createReadStream(`./uploads/${fileName}`) // Create a readable stream to read the uploaded CSV file
+        .pipe(csvParser()); // Pipe the stream to the CSV parser
 
     for await (const record of parser) {
         records.push(record);
@@ -38,12 +42,14 @@ const parseCSV = async () => {
     return records;
 };
 
+// Route to handle file upload and CSV parsing
 app.post('/parseCSV', upload.single('file'), async (req, res) => {
-    const records = await parseCSV();
+    const records = await parseCSV(); // Call the parseCSV function to parse the uploaded CSV file
 
-    res.send(records)
+    res.send(records) // Send the parsed records as a response
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
